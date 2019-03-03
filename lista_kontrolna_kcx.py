36178280bf8bab6881609de31cb33497d28f4624
oljar <oljar@tlen.pl>
2019-03-03 18:07:47 +0100

    creation 02

--- lista_kontrolna_kcx.py
+++ lista_kontrolna_kcx.py
@@ -21,7 +21,7 @@ P=Paragraph('This is a very silly example',style)
 
 elements = []
 
-data= [['Nagłówek','','',''],
+data= [['Nagłówek 1','','',''],
        ['10', '11', '12', '13', '14'],
        ['20', '21', '22', '23', '24'],
        ['30', '31', '32', '33', '34']]
