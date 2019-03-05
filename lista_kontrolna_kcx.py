8aa26de42157805a91e7b85f387aad097aa3a9d5
oljar <oljar@tlen.pl>
2019-03-05 04:21:06 +0100

    tabel_01

--- lista_kontrolna_kcx.py
+++ lista_kontrolna_kcx.py
@@ -22,10 +22,14 @@ P=Paragraph('This is a very silly example',style)
 elements = []
 
 data= [['Nagłówek 1','','',''],
-       ['10', '11', '12', '13', '14'],
-       ['20', '21', '22', '23', '24'],
-       ['30', '31', '32', '33', '34']]
-t=Table(data,5*[1*inch], 4*[1*inch])
+       ['10', '11', '12', '13', '14','15'],
+       ['20', '21', '22', '23', '24','16'],
+       ['30', '31', '32', '33', '34','24'],
+       ['30', '31', '32', '33', '34','24'],]
+
+
+
+t=Table(data,5*[1*inch], 5*[0.4*inch])
 t.setStyle(TableStyle([('ALIGN',(0,0),(-2,-2),'CENTER'),
                        ('TEXTCOLOR',(1,1),(-2,-2),colors.red),
                        ('VALIGN',(0,0),(0,-1),'TOP'),
@@ -35,11 +39,15 @@ t.setStyle(TableStyle([('ALIGN',(0,0),(-2,-2),'CENTER'),
                        ('TEXTCOLOR',(0,-1),(-1,-1),colors.green),
                        ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
                        ('BOX', (0,0), (-1,-1), 0.25, colors.black),
-                       ('SPAN',(0,0),(-1,0)),
+                       ('SPAN',(0,0),(0,2)),
+                       ('SPAN',(1,0),(5,0)),
                        ('FONT',(0,0),(-1,-1),'DejaMono'),
                        ]))
 
-t._argH[1]=1.5*inch
+t._argW[0]=2.5*inch
+t._argW[1] = 0.5*inch
+t._argW[2] = 1*inch
+t._argH[0]=1*inch
 elements.append(t)
 # write the document to disk
 doc.build(elements)
